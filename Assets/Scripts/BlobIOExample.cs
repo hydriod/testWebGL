8f95277b37213881d1da02d15a40bb281ae5b8b0
ysdzm <dokudamichang@gmail.com>
2022-08-06 23:46:18 +0900

    ruccho

---
+++ Assets/Scripts/BlobIOExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using TMPro;
+
+namespace Ruccho.BlobIO.Examples
+{
+    public class BlobIOExample : MonoBehaviour
+    {
+
+        [SerializeField]private TextMeshProUGUI debugText;
+
+        [SerializeField] private string overlayMessage = "Click to select file...";
+        [SerializeField] private string overlayCancelLabel = "Cancel";
+
+        private void Start()
+        {
+            BlobIO.Initialize(overlayMessage, overlayCancelLabel);
+        }
+
+        public void Load()
+        {
+            BlobIO.MakeUpload((f) =>
+            {
+                Debug.Log($"BlobIOExample: File \"{f.Filename}\" uploaded. ");
+                debugText.text = Encoding.UTF8.GetString(f.Data);
+            }, ".txt");
+        }
+
+        public void Save()
+        {
+            BlobIO.MakeDownloadText(debugText.text, "example.txt");
+        }
+    }
+}
