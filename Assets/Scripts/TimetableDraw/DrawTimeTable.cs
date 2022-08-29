7c583afed2a33815943917eedcecbc3a199e539f
hydriod <bibi6548@gmail.com>
2022-08-30 00:32:11 +0900

    時間割UIに科目を追加する機能

--- Assets/Scripts/TimetableDraw/DrawTimeTable.cs
+++ Assets/Scripts/TimetableDraw/DrawTimeTable.cs
@@ -8,7 +8,7 @@ using TMPro;
 namespace TimetableDraw
 {
     /// <summary>
-    /// UI 描画
+    /// 時間割UIに描画するコンポーネント
     /// </summary>
     public class DrawTimeTable : MonoBehaviour
     {
@@ -43,24 +43,29 @@ namespace TimetableDraw
 
             for (int day = 0; day < Day.DAY_MAX - 1; day++)
             {
-                foreach (Subject s in suggester.uiDrawTimeTable[day])
-                {
-                    GameObject go = Instantiate(subjectUiPrefab, dayFrame[day].transform);
-                    // 位置調整
-                    RectTransform t = go.GetComponent<RectTransform>();
-                    t.anchorMax = new Vector2(1f, (14 - (s.startTime - 1)) / 14f);
-                    t.anchorMin = new Vector2(0f, (14 - s.endTime) / 14f);
-                    // 科目名を格納
-                    go.GetComponentInChildren<TMP_Text>().text = s.name;
-                    //
-                    SubjectPanels[day].Add(go);
-                    //break;
-                }
+                DrawDay(day);
+            }
+        }
+        private void DrawDay(int day)
+        {
+            foreach (Subject s in suggester.uiDrawTimeTable[day])
+            {
+                GameObject go = Instantiate(subjectUiPrefab, dayFrame[day].transform);
+                // 位置調整
+                RectTransform t = go.GetComponent<RectTransform>();
+                t.anchorMax = new Vector2(1f, (14 - (s.startTime - 1)) / 14f);
+                t.anchorMin = new Vector2(0f, (14 - s.endTime) / 14f);
+                // 科目名を格納
+                go.GetComponentInChildren<TMP_Text>().text = s.name;
+                // 
+                SubjectPanels[day].Add(go);
+                //break;
             }
         }
 
         /// <summary>
         /// UIイベント用
+        /// 表示をリセットする.
         /// </summary>
         /// <param name="day">削除する曜日</param>
         public void ClearDayTimetable(int day)
@@ -69,6 +74,32 @@ namespace TimetableDraw
             {
                 Destroy(go);
             }
+        }
+
+        /// <summary>
+        /// <para>時間割UIに科目を追加する.</para>
+        /// <para>時間割が衝突するする科目があると上書きされる.</para>
+        /// </summary>
+        /// <param name="subject">追加する科目</param>
+        public void AddSubjectToTimetable(Subject subject)
+        {
+            suggester.uiDrawTimeTable[subject.day].Add(subject);
+
+            foreach (Subject item in suggester.uiDrawTimeTable[subject.day])
+            {
+                // 衝突チェック
+                if (subject.endTime < item.startTime || item.endTime < subject.startTime)
+                {
+                    continue;
+                }
+
+                // 衝突科目を削除
+                suggester.uiDrawTimeTable[subject.day].Remove(item);
+            }
+
+            // 再描画
+            ClearDayTimetable(subject.day);
+            DrawDay(subject.day);
         }
     }
 
